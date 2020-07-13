5d4bf866fd4bffdbfab940ed17c2b177cadec75b
Ramzi Chamcham <ramzichamcham@gmail.com>
2020-07-13 19:47:48 -0400

    Initial commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Ramzi chamcham on 2020-03-02.
+//  Copyright © 2020 Ramzi Chamcham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }.padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+//            .frame(width: 100, alignment: .center)
+            .padding(.all)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
